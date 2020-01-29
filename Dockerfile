afa9b6eeab11fe88ff2fc5fc9a1c1e24549a7154
Laeyoung Chang <gadise@gmail.com>
2020-01-30 00:25:25 +0900

    create Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM node:10
+WORKDIR /workspace
+COPY package*.json ./
+RUN npm install
+COPY . .
+
+EXPOSE 80
+
+CMD [ "node", "server.js" ]
--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require('express')
 const app = express()
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-const port = 8080
+const port = 80
 
 const jhuEdu = require('./api/jhu-edu')
 app.use('/jhu-edu', jhuEdu)
 
-app.get('/', (req, res) => res.send('Hello!'))
+app.get('/', (req, res) => res.send('API Service for tracking the Wuhan Coronavirus!'))
 
-app.listen(port,() => console.log(`Example app listening on port ${port}!`))
+app.listen(port,() => console.log(`API Server listening on port ${port}!`))
