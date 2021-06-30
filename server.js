c002a7d5d8b6ba0821ef0ca509169d1967f4a43a
Laeyoung Chang <laeyoung@comcom.ai>
2021-06-30 14:31:53 +0900

    add cors

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express')
+const cors = require('cors');
 const app = express()
+
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
