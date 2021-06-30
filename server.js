280f0e8db5987635f7799f52566bb85e981cde70
Laeyoung <gadise@gmail.com>
2021-06-30 01:47:32 +0000

    allow all cors

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
-const express = require('express')
-const app = express()
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+const express = require('express');
+const cors = require('cors');
+const app = express();
 
-const port = 80
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-const jhuEdu = require('./api/jhu-edu')
-const kcdc = require('./api/korea-kcdc')
+const port = 3000;
 
-app.use('/jhu-edu', jhuEdu)
-app.use('/kcdc', kcdc)
+const jhuEdu = require('./api/jhu-edu');
+const kcdc = require('./api/korea-kcdc');
 
-app.get('/', (req, res) => res.send('API Service for tracking the COVID-19!'))
+app.use('/jhu-edu', jhuEdu);
+app.use('/kcdc', kcdc);
 
-app.listen(port, () => console.log(`API Server listening on port ${port}!`))
+app.get('/', (req, res) => res.send('API Service for tracking the COVID-19!'));
+
+app.listen(port, () => console.log(`API Server listening on port ${port}!`));
